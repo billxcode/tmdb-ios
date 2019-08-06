873faec5f06061e61679dc54892583a08000d262
Bill Tanthowi Jauhari <bill.tanthowi.j@gmail.com>
2019-08-06 12:08:51 +0700

    Initial Commit

---
+++ MovieCatalogue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieCatalogue
+//
+//  Created by Bill Tanthowi Jauhari on 06/08/19.
+//  Copyright © 2019 Batavia Hack Town. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
